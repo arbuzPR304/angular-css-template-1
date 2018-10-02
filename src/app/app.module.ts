955c5d0e534b943a8aec679c518cb4ec9c563cee
Jasiek <k.kalfas@netkoncept.com>
2018-10-02 23:22:10 +0200

    Zuru pomoc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { VideoSectionComponent } from './video-section/video-section.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    VideoSectionComponent
   ],
   imports: [
     BrowserModule
